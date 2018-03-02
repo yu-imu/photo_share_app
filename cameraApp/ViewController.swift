dd75b77920db9f4806b6c08b24e31ec298a3e21a
imura <imura@synapse.am>
2018-03-03 01:24:03 +0900

    カメラアプリのプロト作りました。

--- cameraApp/ViewController.swift
+++ cameraApp/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var photoImage: UIImageView!
+    
+    @IBAction func cameraLaunchAction(_ sender: Any) {
+        // カメラ機動を確認する
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camrea can be used.")
+            let ipc = UIImagePickerController()
+            ipc.sourceType = .camera
+            ipc.delegate = self
+            // イメージピッカーを表示
+            present(ipc,animated: true, completion: nil)
+        } else {
+            print("Camera is not available.")
+        }
+    }
+    @IBAction func shareAction(_ sender: Any) {
+        if let shareImage = photoImage.image {
+            let shareImages = [shareImage]
+            let controller = UIActivityViewController(activityItems: shareImages, applicationActivities: nil)
+            // ipadの表示対策
+            controller.popoverPresentationController?.sourceView = view
+            // 表示するやつ！
+            present(controller, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        photoImage.image = info["UIImagePickerControllerOriginalImage"] as? UIImage
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
